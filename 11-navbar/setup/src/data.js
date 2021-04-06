0e93379ba20ffea8b2e7641c2908b17c31b14639
Eszter <eszter@codingsans.com>
2021-04-06 13:48:16 +0200

    feat. finish navbar project

--- 11-navbar/setup/src/data.js
+++ 11-navbar/setup/src/data.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import React from 'react';
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 export const links = [
   {
     id: 1,
@@ -26,12 +26,12 @@ export const links = [
     url: '/profile',
     text: 'profile',
   },
-]
+];
 
 export const social = [
   {
     id: 1,
-    url: 'https://www.twitter.com',
+    url: 'https://www.facebook.com',
     icon: <FaFacebook />,
   },
   {
@@ -41,12 +41,12 @@ export const social = [
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
+    url: 'https://www.linkedin.com',
     icon: <FaLinkedin />,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
+    url: 'https://www.behance.net',
     icon: <FaBehance />,
   },
-]
+];
